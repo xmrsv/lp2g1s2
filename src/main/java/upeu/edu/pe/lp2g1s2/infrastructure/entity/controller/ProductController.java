6f1ca4e5c8f59f5d782e1df7820c1ba1527a66c2
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-08-28 12:16:29 -0500

    formatted

--- src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/controller/ProductController.java
+++ src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/controller/ProductController.java
@@ -7,11 +7,11 @@ import upeu.edu.pe.lp2g1s2.app.service.ProductService;
 @Controller
 @RequestMapping("admin/products")
 public class ProductController {
+
     private final ProductService productService;
-    
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-    
-    
+
 }
