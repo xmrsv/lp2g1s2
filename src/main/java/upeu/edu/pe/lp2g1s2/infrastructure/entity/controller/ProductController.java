339e9b450114cd551f8fdf1ccda07289cadd3e71
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-08-28 11:30:04 -0500

    code update

---
+++ src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/controller/ProductController.java
@@ -0,0 +1,17 @@
+package upeu.edu.pe.lp2g1s2.infrastructure.entity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import upeu.edu.pe.lp2g1s2.app.service.ProductService;
+
+@Controller
+@RequestMapping("admin/products")
+public class ProductController {
+    private final ProductService productService;
+    
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    
+    
+}
