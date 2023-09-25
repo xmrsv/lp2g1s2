af612917b854736d2cf247a08a546b1d1bb502bc
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-09-25 12:22:04 -0500

    Products added

--- src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/controller/HomeController.java
+++ src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/controller/HomeController.java
@@ -4,15 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import upeu.edu.pe.lp2g1s2.app.service.ProductService;
 
 @Controller
 @RequestMapping("/home") // Puede ser "/" y vacio para usar la ruta raíz
 public class HomeController {
 
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("titulo",
-                "Bienvenido al ecommerce");
+        model.addAttribute("products",
+                productService.getProducts());
         return "home";
     }
 }
