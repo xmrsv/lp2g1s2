ac87337eab3a51ae623c296335178cb2960d33fa
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-10-11 08:35:18 -0500

    Prettier format

--- src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/adapter/UserRepositoryImpl.java
+++ src/main/java/upeu/edu/pe/lp2g1s2/infrastructure/entity/adapter/UserRepositoryImpl.java
@@ -1,34 +1,31 @@
 package upeu.edu.pe.lp2g1s2.infrastructure.entity.adapter;
 
-
 import org.springframework.stereotype.Repository;
-
 import upeu.edu.pe.lp2g1s2.app.repository.UserRepository;
-import upeu.edu.pe.lp2g1s2.infrastructure.entity.ProductEntity;
 import upeu.edu.pe.lp2g1s2.infrastructure.entity.UserEntity;
 
 @Repository
-public class UserRepositoryImpl implements UserRepository{
+public class UserRepositoryImpl implements UserRepository {
     private final UserCrudRepository userCrudRepository;
 
     public UserRepositoryImpl(UserCrudRepository userCrudRepository) {
         this.userCrudRepository = userCrudRepository;
     }
-      
+
     @Override
     public Iterable<UserEntity> getUsers() {
-        //metodo propio de Spring 
+        // metodo propio de Spring
         return userCrudRepository.findAll();
     }
 
     @Override
     public UserEntity saveUser(UserEntity userEntity) {
-       return userCrudRepository.save(userEntity);
+        return userCrudRepository.save(userEntity);
     }
 
     @Override
     public void deleteUserById(Integer id) {
-       userCrudRepository.deleteById(id);
+        userCrudRepository.deleteById(id);
     }
-    
+
 }
