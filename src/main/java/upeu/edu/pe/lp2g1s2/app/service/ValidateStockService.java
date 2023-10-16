da1f0f1acc2ebe89d10034c28c6e8db5a7f76638
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-10-15 23:21:48 -0500

    Code is more readable now

--- src/main/java/upeu/edu/pe/lp2g1s2/app/service/ValidateStockService.java
+++ src/main/java/upeu/edu/pe/lp2g1s2/app/service/ValidateStockService.java
@@ -4,72 +4,34 @@ import java.util.List;
 import upeu.edu.pe.lp2g1s2.infrastructure.entity.ProductEntity;
 import upeu.edu.pe.lp2g1s2.infrastructure.entity.StockEntity;
 
-/**
- * Servicio para validar y actualizar el saldo de un producto en el inventario.
- */
 public class ValidateStockService {
 
     private final StockService stockService;
 
-    /**
-     * Constructor de ValidateStockService.
-     *
-     * @param stockService Servicio de gestión del inventario.
-     */
     public ValidateStockService(StockService stockService) {
         this.stockService = stockService;
     }
 
-    /**
-     * Verifica si existe un saldo de inventario para un producto.
-     *
-     * @param productEntity Entidad de producto para la cual verificar el saldo.
-     * @return `true` si existe saldo, `false` si no.
-     */
-    private boolean existBalance(ProductEntity productEntity) {
-        List<StockEntity> stockList = stockService.getStockByProductEntity(productEntity);
+    private boolean existBalance(ProductEntity product) {
+        List<StockEntity> stockList = stockService.getStockByProduct(product);
         return !stockList.isEmpty();
     }
 
-    /**
-     * Calcula y actualiza el saldo de un producto en el inventario.
-     *
-     * @param stock StockEntity que contiene información de entradas, salidas y
-     * producto.
-     * @return El mismo objeto StockEntity con el saldo actualizado.
-     */
     public StockEntity calculateBalance(StockEntity stock) {
-        if (stock.getEntries() != 0) {
-            calculateBalanceForEntries(stock);
-        } else {
-            calculateBalanceForExits(stock);
+        Integer balance = 0;
+        if (existBalance(stock.getProductEntity())) {
+            List<StockEntity> stockList = stockService.getStockByProduct(stock.getProductEntity());
+            balance = stockList.get(stockList.size() - 1).getBalance();
         }
+
+        if (stock.getEntries() != 0) {
+            balance += stock.getEntries();
+        } else {
+            balance -= stock.getExits();
+        }
+
+        stock.setBalance(balance);
         return stock;
     }
 
-    /**
-     * Calcula el saldo en función de las entradas de producto.
-     *
-     * @param stock StockEntity que contiene información de entradas y producto.
-     */
-    private void calculateBalanceForEntries(StockEntity stock) {
-        if (existBalance(stock.getProductEntity())) {
-            List<StockEntity> stockList = stockService.getStockByProductEntity(stock.getProductEntity());
-            Integer balance = stockList.get(stockList.size() - 1).getBalance();
-            stock.setBalance(balance + stock.getEntries());
-        } else {
-            stock.setBalance(stock.getEntries());
-        }
-    }
-
-    /**
-     * Calcula el saldo en función de las salidas de producto.
-     *
-     * @param stock StockEntity que contiene información de salidas y producto.
-     */
-    private void calculateBalanceForExits(StockEntity stock) {
-        List<StockEntity> stockList = stockService.getStockByProductEntity(stock.getProductEntity());
-        Integer balance = stockList.get(stockList.size() - 1).getBalance();
-        stock.setBalance(balance - stock.getExits());
-    }
 }
